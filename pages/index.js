f5b293832497271dd7f66798a9006c5d6a54dd52
aritroCoder <ogcodinggod@gmail.com>
2022-12-11 11:54:34 +0530

    simple-next-app

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
+
+  React.useEffect(() => {
+    console.log(styles)
+  }, [])
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>This is the home page</p>
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Home Page</h1>
+      <p style={{margin: "10px", padding: "10px"}}>
+        This is the home page
+      </p>
     </div>
-  )
+  );
 }
